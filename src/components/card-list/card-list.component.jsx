263d12c63fc9b8fb19bd0faf9bd91c503081b12e
Oguz Alp Saglam <oguzalp7@github.com>
2023-05-23 16:32:12 +0300

    add card list css

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,17 +1,17 @@
 import { Component } from "react";
-
+import './card-list.styles.css';
 
 // begining of card list class
 class CardList extends Component{
     render() {
         
         const { monsters } = this.props;
-
+        
         /*
         Apply same beheaviour in the CardList class. The behaviour is deleted from the App.js        
         */
         return(
-            <div>
+            <div className="card-list">
                 {
                     /*
                     map function: makes list/arrays mapped. 
@@ -19,7 +19,14 @@ class CardList extends Component{
                     elements in an array. Bellisimo :) 
                     */
                     monsters.map((monster) => (
-                        <h1 key={monster.id}> { monster.name }</h1>
+                        <div className='card-container' key={monster.id}> 
+                            <img 
+                                alt={'monster ${monster.name}'} 
+                                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                            />
+                            <h2>{monster.name}</h2>
+                            <p>{monster.email}</p>
+                        </div>
                     ))
                 }
             </div>
